37956549cda692b0a8f6a06cbf53eaa78e843edb
Lennart Heim <lennart.heim@rwth-aachen.de>
2018-08-20 15:55:51 +0200

    draft of tx and rx notifier

--- start-AP.py
+++ start-AP.py
@@ -8,7 +8,12 @@ import json
 import logging
 import glob
 
+import sys
+from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 
+
+
+PORT = 5005
 
 
 formatter = "%(asctime)s - %(name)s (%(threadName)-10s) - %(levelname)s - %(message)s"
@@ -21,17 +26,21 @@ thread 1:
 iperf connection (logged using .json) add waiting time in the beginning to adjust for TCP start
 
 '''
-def thread_iperf(sync_event, stop_event, target_ip, interval, length, logname):
+def thread_iperf_tx(sync_event, stop_event, target_ip, interval, length, logname, reverse):
 
-    logger.info('Starting Thread 1!')
+    logger.info("Starting Thread 'iperf tx'!")
 
     # call a subprocess for iperf
     args = ['iperf3', '-c', target_ip, '-i', interval, '-t', length, '-J', '--logfile', '%s_iperf.json' %logname]
+    if reverse:
+        args.append('-R')
     logger.debug(args)
 
     output = subprocess.Popen(args)
 
     logger.debug(output)
+
+    notify_receiver(time.time(), target_ip, length, logname, reverse)
 
     # set an event every second to notify other threads to save MCS and sweep_dump
     for i in range(0, int(length)):
@@ -39,12 +48,13 @@ def thread_iperf(sync_event, stop_event, target_ip, interval, length, logname):
         time.sleep(1)
 
 
-    logger.info('Setting Stop Event!')
+    logger.info("Setting Stop Event!")
     stop_event.set()
-    logger.debug('Setting Sync Event one last time!')
+    logger.debug("Setting Sync Event one last time!")
     sync_event.set() # run one last time
 
-    logger.info('Finished Thread 1!')
+    logger.info("Finished Thread 'iperf tx'!")
+    return 0
 
 
 
@@ -58,9 +68,9 @@ thread 2:
 log MCS with 'iw dev wlan2 link'
 
 '''
-def thread_mcs(sync_event, stop_event, logname):
+def thread_mcs(sync_event, stop_event, logname, role):
 
-    logger.info('Starting Thread 2!')
+    logger.info("Starting Thread 'MCS'!")
 
 
     iw_dict = {} # create dict
@@ -68,6 +78,7 @@ def thread_mcs(sync_event, stop_event, logname):
 
     iw_dict = {}
     iw_dict['filname'] = "%s_MCS.json" %logname
+    iw_dict['role'] = role
     iw_dict['start_time'] = time.time()
     iw_dict['data'] = []
 
@@ -77,10 +88,9 @@ def thread_mcs(sync_event, stop_event, logname):
 
         sync_event.wait() # Blocks until the flag becomes true.
 
-        timestamp = time.time()
 
         iw_dict['data'].append({})
-        iw_dict['data'][-1]['time'] = timestamp
+        iw_dict['data'][-1]['time'] = time.time()
         iw_dict['data'][-1]['interval'] = i
         i += 1
 
@@ -102,7 +112,7 @@ def thread_mcs(sync_event, stop_event, logname):
             iw_dict['data'][-1]['MCS'] = output[output.find('MCS')+4:-1]
 
         except subprocess.CalledProcessError:
-            logger.info('Error while checking MCS.')
+            logger.info("Error while checking MCS.")
             iw_dict['data'][-1]['bitrate'] = 'err'
             iw_dict['data'][-1]['MCS'] = 'err'
 
@@ -120,7 +130,8 @@ def thread_mcs(sync_event, stop_event, logname):
         json.dump(iw_dict, iw_file, indent='\t')
 
 
-    logger.info('Finished Thread 2!')
+    logger.info("Finished Thread 'MCS'!")
+    return 0
 
 
 
@@ -133,15 +144,16 @@ def thread_mcs(sync_event, stop_event, logname):
 thread 3:
 log sector sweep 'cat /sys/kernel/debug/ieee80211/phy2/wil6210/sweep_dump_cur'
 '''
-def thread_sweep(sync_event, stop_event, logname):
+def thread_sweep(sync_event, stop_event, logname, role):
 
-    logger.info('Starting Thread 3!')
+    logger.info("Starting Thread 'sweep-dump'!")
     sweeps_log = 'Start sweep_dump log:\n'
 
     i = 0
 
     sweep_dict = {}
     sweep_dict['filname'] = "%s_sweep-dump.json" %logname
+    sweep_dict['role'] = role
     sweep_dict['start_time'] = time.time()
     sweep_dict['data'] = []
 
@@ -156,7 +168,7 @@ def thread_sweep(sync_event, stop_event, logname):
 
                 first_line = sweep_dump.readline()
 
-                logger.debug('First line %s' %first_line)
+                logger.debug("First line %s" %first_line)
 
                 sweep_dict['data'].append({})
                 sweep_dict['data'][-1]['time'] = time.time()
@@ -166,7 +178,7 @@ def thread_sweep(sync_event, stop_event, logname):
 
                 for line in sweep_dump:
                     if line.find('[') is not -1: # skip first and last line
-                        logger.debug('Current line %s' %line)
+                        logger.debug("Current line %s" %line)
                         sweep_dict['data'][-1]['dump'].append({})
                         sweep_dict['data'][-1]['dump'][-1]['sec'] = line[line.find('sec:')+5:line.find('rssi')-1].strip()
                         sweep_dict['data'][-1]['dump'][-1]['rssi'] = line[line.find('rssi:')+6:line.find('snr')-1].strip()
@@ -187,13 +199,14 @@ def thread_sweep(sync_event, stop_event, logname):
         if stop_event.is_set():
             break
 
-    logger.debug('Writing sweep-dump to .json')
+    logger.debug("Writing sweep-dump to .json")
 
     with open("%s_sweep-dump.json" %logname, "w+") as sweep_file:
         json.dump(sweep_dict, sweep_file, indent='\t')
 
 
-    logger.info('Finished Thread 3!')
+    logger.info("Finished Thread 'sweep-dump'!")
+    return 0
 
 
 
@@ -205,40 +218,97 @@ TODO
 ------------------------------------------
 sync_clients:
 '''
+def notify_receiver(time, target_ip, length, logname, reversed):
 
-def sync_client():
+    notificationSocket = socket(AF_INET, SOCK_DGRAM)
 
-    import sys
-    from socket import socket, AF_INET, SOCK_DGRAM
+    logger.info("Notifying receiver (IP %s via port %d)." %(target_ip, PORT))
 
-    SERVER_IP   = '192.168.8.102'
-    PORT_NUMBER = 5000
+    notification = {}
+    notification['notifier time'] = time
+    notification['length'] = length
+    notification['logname'] = logname
+    if reversed: #
+        notification['role'] = 'tx'
+    else:
+        notification['role'] = 'rx'
+
+    notificationSocket.sendto(json.dumps(notification).encode('utf-8'), (target_ip, PORT))
+
+
+
+def listener_receiver():
+
+    threads = []
+    sync_event = Event()
+    stop_event = Event()
+
     SIZE = 1024
-    logger.info ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
+    hostName = gethostbyname('0.0.0.0')
 
-    mySocket = socket( AF_INET, SOCK_DGRAM )
-    myMessage = "Hello!"
-    myMessage1 = "42"
-    i = 0
-    while i < 10:
-        mySocket.sendto(myMessage.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
-        i = i + 1
+    notificationSocket = socket(AF_INET, SOCK_DGRAM)
+    notificationSocket.bind((hostName, PORT))
 
-    mySocket.sendto(myMessage1.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
+    logger.info("Waiting for notification ...")
 
-    # sys.exit()
+    while True:
+        (data,addr) = notificationSocket.recvfrom(SIZE)
+
+        try:
+            notification = json.loads(data.decode('utf-8'))
+        except ValueError:
+            Logger.warning("Received faulty data: ValueError.")
+            break
+
+        logger.info("Received a notifcation: %s" %notification)
+
+        logger.info("Received time %f:" %notification['notifier time'])
+        logger.info("My time %f:" %time.time())
+
+
+
+        # SWEEP thread
+        threads.append(Thread(target=thread_sweep, args=[sync_event, stop_event, notification['logname'], notification['role']]))
+        threads[-1].start()
+
+
+        # set event
+        # set an event every second to notify other threads to save MCS and sweep_dump
+        for i in range(0, int(notification['length'])):
+            sync_event.set() # notify the other threads
+            time.sleep(1)
+
+        logger.info("Setting Stop Event!")
+        stop_event.set()
+        logger.debug("Setting Sync Event one last time!")
+        sync_event.set() # run one last time
+
+        for thread in threads:
+            thread.join()
+
+        stop_event.clear()
+        logger.info("Threads joined - finished handling notifcation. Waiting for a new notification ...")
+
+
 
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Starting a measurement. Recording everything with iperf3, MCS index, and a sweep dump every second.')
+    parser = argparse.ArgumentParser(description="Starting a measurement. Recording everything with iperf3, MCS index, and a sweep dump every second.")
 
 
-    parser.add_argument('--target', '-c',   help='IP address of iperf3 server', required=True, type=str)
-    parser.add_argument('--interval', '-i', help='set length of iperf3 intervals', default='1', type=str)
-    parser.add_argument('--length', '-t',   help='length of the measurement in seconds', default='120', type=str)
-    parser.add_argument('--logname', '-l',  help='name of the logfile', required=True, type=str)
-    parser.add_argument('--verbose', '-v',  help='enable verbose logging', action='store_true')
+    parser.add_argument('--role', '-r',  help="set the role (transmitter or receiver)", choices=['tx', 'rx'], required=True)
+
+
+    parser.add_argument('--target', '-c',   help="IP address of iperf3 server", required=True, type=str)
+    parser.add_argument('--interval', '-i', help="set length of iperf3 intervals", default='1', type=str)
+    parser.add_argument('--length', '-t',   help="length of the measurement in seconds", default='120', type=str)
+    parser.add_argument('--logname', '-l',  help="name of the logfile", required=True, type=str)
+    parser.add_argument('--reverse', '-R',  help="reverse the direction of the iperf test (server sends)", action='store_true')
+
+
+
+    parser.add_argument('--verbose', '-v',  help="enable verbose logging", action='store_true')
 
 
     args = parser.parse_args()
@@ -251,37 +321,51 @@ def main():
         logging.basicConfig(level=logging.INFO, format=formatter)
 
 
-
     # check if file already exists
     if glob.glob('%s*' %args.logname):
-        logger.warning('Group of files starting with "%s" already exist!'%args.logname)
+        logger.warning("Group of files starting with '%s' already exist!" %args.logname)
         return -1
 
 
 
+    if args.role == 'tx':
 
-    sync_event = Event()
-    stop_event = Event()
+        threads = []
+        sync_event = Event()
+        stop_event = Event()
+        # MCS
+        threads.append(Thread(target=thread_mcs, args=[sync_event, stop_event, args.logname, args.role]))
+        threads[-1].start()
 
+        # SWEEP
+        threads.append(Thread(target=thread_sweep, args=[sync_event, stop_event, args.logname, args.role]))
+        threads[-1].start()
 
-    threads = []
-
-    threads.append(Thread(target=thread_iperf, args = [sync_event, stop_event, args.target, args.interval, args.length, args.logname]))
-    threads[-1].start() # refers to the last element (-2 to the second last)
-
-    threads.append(Thread(target=thread_mcs, args=[sync_event, stop_event, args.logname]))
-    threads[-1].start()
-
-    threads.append(Thread(target=thread_sweep, args=[sync_event, stop_event, args.logname]))
-    threads[-1].start()
-
-
-    # join threads
-    for thread in threads:
-        thread.join()
+        # IPERF TX
+        threads.append(Thread(target=thread_iperf_tx, args = [sync_event, stop_event, args.target, args.interval, args.length, args.logname, args.reverse]))
+        threads[-1].start()
 
 
-    logger.info("All threads joined. All done.")
+        # join threads
+        for thread in threads:
+            thread.join()
+        logger.info("All threads joined. All done.")
+
+
+
+    elif args.role == 'rx':
+        logger.info("Please run 'iperf3 -s' on the rx side manually.")
+        logger.info("Starting a listener ...")
+        try:
+            listener_receiver()
+        except KeyboardInterrupt:
+            logger.info("Keyboard Interrupt: Exiting ...")
+            sys.exit()
+        # start scripts with different logname get logname from control signal
+
+    else:
+        logger.warning("Role not specified.")
+
 
     return 0
 
